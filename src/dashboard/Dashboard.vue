401651abcbb85817d2dc649e1a0afbc81c7bf761
HaoVA <hi@haova.me>
2024-09-13 05:19:33 +0700

    fix: transition css

--- src/dashboard/Dashboard.vue
+++ src/dashboard/Dashboard.vue
@@ -187,15 +187,3 @@ onUnmounted(() => {
     </div>
   </div>
 </template>
-
-<style>
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-</style>
