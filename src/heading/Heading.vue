0680f6e52c23024de98e4b1ae30df633bdf2928e
HaoVA <hi@haova.me>
2024-09-14 09:01:12 +0700

    feat: Heading component

---
+++ src/heading/Heading.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  level: {
+    type: Number,
+    default: 1,
+  },
+});
+
+const fontSizes = {
+  1: 'text-2xl',
+  2: 'text-xl',
+  3: 'text-lg',
+  4: 'text-base',
+  5: 'text-sm',
+  6: 'text-xs',
+};
+</script>
+
+<template>
+  <component :is="`h${level}`" :class="`${fontSizes[level]} font-semibold mb-4`"><slot></slot></component>
+</template>
---
+++ src/views/Headings.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Heading from '../heading/Heading.vue';
+</script>
+
+<template>
+  <label class="mb-2">Level 1</label>
+  <Heading>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Heading>
+
+  <label class="mb-2">Level 2</label>
+  <Heading level="2">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Heading>
+
+  <label class="mb-2">Level 3</label>
+  <Heading level="3">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Heading>
+
+  <label class="mb-2">Level 4</label>
+  <Heading level="4">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Heading>
+
+  <label class="mb-2">Level 5</label>
+  <Heading level="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Heading>
+
+  <label class="mb-2">Level 6</label>
+  <Heading level="6">Lorem ipsum dolor sit amet, consectetur adipiscing elit</Heading>
+</template>
