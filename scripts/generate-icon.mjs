bece1d2afc9ea73b027a413a53413e37638aa4be
HaoVA <hi@haova.me>
2024-11-14 21:52:14 +0700

    feat: heroicons as icons

---
+++ scripts/generate-icon.mjs
@@ -0,0 +1,38 @@
+import camelcase from 'camelcase';
+import { existsSync, mkdirSync, readdirSync, readFileSync, rmSync, writeFileSync } from 'node:fs';
+
+import { basename, extname, join } from 'node:path';
+
+function getIcons() {
+  const ls = readdirSync(`./heroicons/optimized/24/solid`);
+
+  return ls.map((i) => basename(i, extname(i)));
+}
+
+const data = getIcons();
+const template = readFileSync('./templates/Icon.vue').toString();
+
+if (!existsSync('./src/icons')) mkdirSync('./src/icons', { recursive: true });
+
+let cjsIndex = '';
+let mjsIndex = '';
+
+for (const name of data) {
+  const componentName = camelcase(name, { pascalCase: true }) + 'Icon';
+  let content = template;
+
+  let outline = readFileSync(join('./heroicons/optimized/24/outline', name + '.svg')).toString();
+  let solid = readFileSync(join('./heroicons/optimized/24/solid', name + '.svg')).toString();
+
+  content = content.replace(`<!-- outline -->`, outline);
+  content = content.replace(`<!-- solid -->`, solid);
+
+  writeFileSync(join('./src/icons', componentName + '.vue'), content);
+
+  cjsIndex += `module.exports.${componentName} = require("./${componentName}.cjs");\n`;
+  mjsIndex += `export { default as ${componentName} } from './${componentName}.mjs';\n`;
+}
+
+if (!existsSync('./dist/icons')) mkdirSync('./dist/icons', { recursive: true });
+writeFileSync('./dist/icons/index.cjs', cjsIndex);
+writeFileSync('./dist/icons/index.mjs', mjsIndex);
---
+++ templates/Icon.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps(['outlined']);
+</script>
+
+<template>
+  <template v-if="outlined">
+    <!-- outline -->
+  </template>
+  <template v-else>
+    <!-- solid -->
+  </template>
+</template>
