7cd1c609378fea08c51d2ba2f8d29467fc83b7f1
HaoVA <hi@haova.me>
2024-11-17 12:43:47 +0700

    feat: icon search

--- src/test/Playground.vue
+++ src/test/Playground.vue
@@ -4,19 +4,19 @@ import CODE from '../code.ts';
 import { getCurrentInstance, onMounted } from 'vue';
 import CodePreview from './CodePreview.vue';
 
-defineProps(['id', 'preview']);
+defineProps(['id']);
 </script>
 
 <template>
   <div class="border rounded-lg overflow-hidden">
     <Tabs>
-      <template #Preview v-if="preview !== false">
+      <template #Preview>
         <div class="p-4">
           <slot></slot>
         </div>
       </template>
 
-      <template #Code v-if="preview === false && id">
+      <template #Code v-if="id">
         <CodePreview class="!rounded-none">
           <pre>{{ CODE[id] }}</pre>
         </CodePreview>
