cd21747e504bee49ca54258f9622d24bb3587980
HaoVA <hi@haova.me>
2024-11-18 15:12:13 +0700

    feat: form

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,32 +1,85 @@
 <script setup>
-import Button from '../button/Button.vue';
-import Input from '../input/Input.vue';
-import Field from '../field/Field.vue';
-import Heading from '../heading/Heading.vue';
-
-function handleSubmit(e) {
-  e.preventDefault();
-  return false;
-}
+import ArrowRightCircleIcon from '../icons/ArrowRightCircleIcon.vue';
+import CheckIcon from '../icons/CheckIcon.vue';
+import Playground from '../test/Playground.vue';
 </script>
 
 <template>
-  <Heading level="2">Basic</Heading>
-  <form @submit="handleSubmit" class="flex flex-col gap-4 mb-4">
-    <Field label="Username" v-slot="{ id }">
-      <Input :id="id" name="username" modelValue="sample user" />
-    </Field>
-    <Field label="Password" v-slot="{ id }">
-      <Input :id="id" name="password" type="password" />
-    </Field>
-    <Button>Login</Button>
-  </form>
-
-  <Heading level="2">Inline</Heading>
-  <form @submit="handleSubmit">
-    <Field v-slot="{ id }" class="flex">
-      <Input :id="id" name="search" />
-      <Button>Search</Button>
-    </Field>
-  </form>
+  <div class="grid grid-cols-1 gap-4">
+    <Playground id="form">
+      <!-- FORM -->
+      <div class="flex flex-col gap-4">
+        <!-- FIELD -->
+        <div class="flex flex-col gap-2">
+          <label for="inp01" class="text-sm leading-4 font-medium">Label</label>
+          <!-- INPUT GROUP -->
+          <div class="flex gap-2">
+            <!-- INPUT -->
+            <input
+              id="inp01"
+              class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
+              placeholder="Text Input"
+              spellcheck="false"
+            />
+            <!-- END INPUT -->
+            <!-- ICON WITH TEXT BUTTON -->
+            <button
+              class="text-sm leading-4 font-medium p-2.5 rounded inline-flex items-center gap-1.5 bg-primary text-background"
+            >
+              <ArrowRightCircleIcon class="w-5 h-5" />
+              <span class="inline-block p-0.5">Button</span>
+            </button>
+            <!-- END ICON WITH TEXT BUTTON -->
+          </div>
+          <!-- INPUT GROUP -->
+          <span class="inline-block italic text-xs">This is a help message</span>
+        </div>
+        <!-- END FIELD -->
+        <!-- FIELD -->
+        <div class="flex flex-col gap-2">
+          <label for="inp01" class="text-sm leading-4 font-medium">Label</label>
+          <!-- INPUT -->
+          <input
+            id="inp01"
+            class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
+            placeholder="Text Input"
+            spellcheck="false"
+          />
+          <!-- END INPUT -->
+        </div>
+        <!-- END FIELD -->
+        <!-- FIELD -->
+        <div class="flex flex-col gap-2">
+          <!-- CHECKBOX -->
+          <div class="text-sm leading-4 inline-flex items-center">
+            <!-- VALUE -->
+            <input id="cbx01" class="peer hidden" type="checkbox" />
+            <!-- END VALUE -->
+            <label for="cbx01" class="inline-block peer-checked:hidden border border-primary w-4 h-4 rounded"> </label>
+            <label
+              for="cbx01"
+              class="hidden peer-checked:inline-block border border-primary bg-primary text-background w-4 h-4 rounded"
+            >
+              <CheckIcon class="w-3.5 h-3.5" />
+            </label>
+            <label for="cbx01" class="inline-block p-0.5 pl-1.5">This is a checkbox</label>
+          </div>
+          <!-- END CHECKBOX -->
+        </div>
+        <!-- END FIELD -->
+        <!-- FIELD -->
+        <div class="flex gap-4">
+          <!-- TEXT BUTTON -->
+          <button
+            class="text-sm leading-4 font-medium p-2.5 rounded inline-flex items-center gap-1.5 bg-primary text-background"
+          >
+            <span class="inline-block p-0.5">Submit</span>
+          </button>
+          <!-- END TEXT BUTTON -->
+        </div>
+        <!-- END FIELD -->
+      </div>
+      <!-- END FORM -->
+    </Playground>
+  </div>
 </template>
--- src/views/Input.vue
+++ src/views/Input.vue
@@ -9,83 +9,103 @@ const tmp = ref('');
 <template>
   <div class="grid grid-cols-1 gap-4">
     <Playground id="text-input">
+      <!-- TEXT INPUT -->
       <input
         class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
         placeholder="Text Input"
         spellcheck="false"
       />
+      <!-- END TEXT INPUT -->
     </Playground>
 
     <Playground id="number-input">
+      <!-- NUMBER INPUT -->
       <input
         type="number"
         class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
         placeholder="Number Input"
       />
+      <!-- END NUMBER INPUT -->
     </Playground>
 
     <Playground id="date-input">
+      <!-- DATE INPUT -->
       <input
         type="date"
         class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
         placeholder="Date Input"
       />
+      <!-- END DATE INPUT -->
     </Playground>
 
     <Playground id="file-input">
+      <!-- FILE INPUT -->
       <input
         type="file"
         class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
         placeholder="File Input"
       />
+      <!-- END FILE INPUT -->
     </Playground>
 
     <Playground id="mask-number-input">
+      <!-- MASKED INPUT -->
       <input
         class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
         placeholder="Mask Number Input"
         v-mask="Number"
       />
+      <!-- END MASKED INPUT -->
     </Playground>
 
-    <Playground id="mask-number-input">
+    <Playground id="mask-number-2-input">
+      <!-- MASKED INPUT -->
       <input
         class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
         placeholder="(+00) 000 000 000"
         v-mask="'(+00) 000 000 000'"
       />
+      <!-- END MASKED INPUT -->
     </Playground>
 
     <Playground id="mask-date-input">
+      <!-- MASKED INPUT -->
       <input
         class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
         placeholder="yyyy-mm-dd"
         v-mask="'yyyy-mm-dd'"
       />
+      <!-- END MASKED INPUT -->
     </Playground>
 
     <Playground id="mask-time-input">
+      <!-- MASKED INPUT -->
       <input
         class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
         placeholder="hh:ii:ss"
         v-mask="'hh:ii:ss'"
       />
+      <!-- END MASKED INPUT -->
     </Playground>
 
     <Playground id="mask-regex-input">
+      <!-- MASKED INPUT -->
       <input
         class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
         placeholder="Decimal and Space"
         v-mask="/^[\d\s]+$/"
       />
+      <!-- END MASKED INPUT -->
     </Playground>
 
     <Playground id="mask-color-input">
+      <!-- MASKED INPUT -->
       <input
         class="outline outline-1 -outline-offset-1 bg-background focus:outline-muted w-full px-3 py-2 text-sm leading-6 rounded"
         placeholder="Hex Color"
         v-mask="'#xxxxxx'"
       />
+      <!-- END MASKED INPUT -->
     </Playground>
   </div>
 </template>
